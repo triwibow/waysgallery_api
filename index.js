6897742832d40d2795cdcee7bc570ea898b27ad6
Tri Wibowo <triw@oninyon.com>
2023-09-06 14:58:46 +0700

    23

--- index.js
+++ index.js
@@ -4,15 +4,13 @@ const app = express();
 const cors = require('cors')
 const port = 5000;
 
-const routerV1 = require('./src/routes/routeV1');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('uploads'));
-app.use(cors({
-  origin:'http://wagallery.netlify.app'
-}));
+app.use(cors());
 
+const routerV1 = require('./src/routes/routeV1');
 
 app.use('/api/v1/', routerV1);
 
