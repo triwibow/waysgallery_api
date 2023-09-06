5e904c378d668ca2031fc6ded114ea9257b65c64
Tri Wibowo <triw@oninyon.com>
2023-09-06 14:50:16 +0700

    rao

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('uploads'));
 app.use(cors({
-  origin:'https://wagallery.netlify.app'
+  origin:'http://wagallery.netlify.app'
 }));
 
 
