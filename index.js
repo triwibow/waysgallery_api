3073f14e4f96f1e0d3752a2556b69eb85ec4fc27
tri w <triw@oninyon.com>
2023-09-06 20:18:13 +0700

    -dd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors')
-const port = 5000;
+const port = 8000;
 
 app.use(cors());
 app.use(express.json());
